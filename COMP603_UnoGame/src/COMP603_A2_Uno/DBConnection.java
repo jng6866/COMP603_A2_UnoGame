d7dffd40062d6829715e0a5e173bae2ae5691d3e
Mufasa <kamish.mustafa@gmail.com>
2024-10-23 15:34:53 +1300

    missing file

---
+++ COMP603_UnoGame/src/COMP603_A2_Uno/DBConnection.java
@@ -0,0 +1,91 @@
+package COMP603_A2_Uno;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class DBConnection {
+
+
+    private static final String URL = "jdbc:derby://localhost:1527/GameDB";
+
+    private static final String USER_NAME = "test";
+    private static final String PASSWORD = "test"; 
+    Connection conn;
+
+    public DBConnection() {
+        establishConnection();
+    }
+
+    public static void main(String[] args) {
+        DBConnection dbConnection = new DBConnection();
+        System.out.println(dbConnection.getConnection());
+    }
+
+    public Connection getConnection() {
+        return this.conn;
+    }
+
+
+    public void establishConnection() {
+        if (this.conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+                System.out.println(URL + " Get Connected Successfully ....");
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public void closeConnections() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public ResultSet queryDB(String sql) {
+
+        Connection connection = this.conn;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return resultSet;
+    }
+
+    public void updateDB(String sql) {
+
+        Connection connection = this.conn;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}
